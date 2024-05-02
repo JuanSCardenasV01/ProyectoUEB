2b016111504bdcfeff3f1fc3dd8b70af9c45a766
biblioteca <biblioteca@UBACBIBP-080.unbosque.edu.co>
2024-05-02 10:40:18 -0500

    GUI ADMIN

--- ProyectoProgrmacion1/src/co/edu/unbosque/controller/Controlador.java
+++ ProyectoProgrmacion1/src/co/edu/unbosque/controller/Controlador.java
@@ -31,6 +31,16 @@ public class Controlador implements ActionListener {
 		vF.getVenLog().getBtnRegister().setActionCommand("btnRegister");
 		vF.getVenRe().getBtnBack().addActionListener(this);
 		vF.getVenRe().getBtnBack().setActionCommand("btnBack");
+		vF.getVenPA().getBtnLogout().addActionListener(this);
+		vF.getVenPA().getBtnLogout().setActionCommand("btnLogout");
+		vF.getVenPA().getBtn1().addActionListener(this);
+		vF.getVenPA().getBtn1().setActionCommand("btn1");
+		vF.getVenPA().getBtn2().addActionListener(this);
+		vF.getVenPA().getBtn2().setActionCommand("btn2");
+		vF.getVenPA().getBtn3().addActionListener(this);
+		vF.getVenPA().getBtn3().setActionCommand("btn3");
+		vF.getVenPA().getBtn4().addActionListener(this);
+		vF.getVenPA().getBtn4().setActionCommand("btn4");
 
 	}
 
@@ -54,10 +64,52 @@ public class Controlador implements ActionListener {
 			break;
 		}
 		case "btnBack":{
-			vF.getVenLog().setVisible(true);
 			vF.getVenRe().setVisible(false);
+			vF.getVenLog().setVisible(true);
 			break;
 		}
+		case "btnLogout":{
+			vF.getVenPA().setVisible(false);
+			vF.getVenLog().setVisible(true);
+			break;
+		}
+		case "btn1":{
+            // Ocultar todos los paneles
+            vF.getVenPA().getPanel2().setVisible(false);
+            vF.getVenPA().getPanel3().setVisible(false);
+            vF.getVenPA().getPanel4().setVisible(false);
+            // Mostrar el panel correspondiente
+            vF.getVenPA().getPanel1().setVisible(true);
+            break;
+        }
+        case "btn2":{
+            // Ocultar todos los paneles
+            vF.getVenPA().getPanel1().setVisible(false);
+            vF.getVenPA().getPanel3().setVisible(false);
+            vF.getVenPA().getPanel4().setVisible(false);
+            // Mostrar el panel correspondiente
+            vF.getVenPA().getPanel2().setVisible(true);
+            break;
+        }
+        case "btn3":{
+            // Ocultar todos los paneles
+            vF.getVenPA().getPanel1().setVisible(false);
+            vF.getVenPA().getPanel2().setVisible(false);
+            vF.getVenPA().getPanel4().setVisible(false);
+            // Mostrar el panel correspondiente
+            vF.getVenPA().getPanel3().setVisible(true);
+            break;
+        }
+        case "btn4":{
+            // Ocultar todos los paneles
+            vF.getVenPA().getPanel1().setVisible(false);
+            vF.getVenPA().getPanel2().setVisible(false);
+            vF.getVenPA().getPanel3().setVisible(false);
+            // Mostrar el panel correspondiente
+            vF.getVenPA().getPanel4().setVisible(true);
+            break;
+        }
+		
 		
 		}
 
--- ProyectoProgrmacion1/src/co/edu/unbosque/view/VentanaPrincipalAdmin.java
+++ ProyectoProgrmacion1/src/co/edu/unbosque/view/VentanaPrincipalAdmin.java
@@ -17,100 +17,241 @@ import javax.swing.JTextField;
 public class VentanaPrincipalAdmin extends JFrame {
 
 	private JPanel panelAdmin;
-	private JPanel panelActions;
-	private JLabel textTittle;
-	private JLabel textUser;
-	private JLabel textPassword;
+	private JPanel panelSidebar;
+	private JPanel panel1;
+	private JPanel panel2;
+	private JPanel panel3;
+	private JPanel panel4;
+	private JLabel textAdmin;
 	private JLabel logo;
-	private JTextField fieldUser;
-	private JPasswordField fieldPassword;
-	private JButton btnLogin;
-	private JButton btnRegister;
+	private JButton btnLogout;
+	private JButton btn1;
+	private JButton btn2;
+	private JButton btn3;
+	private JButton btn4;
 	private JSeparator line1;
-	private JSeparator line2;
 
 	public VentanaPrincipalAdmin() {
-		setSize(700, 700);
+		setSize(800, 500);
 		setLayout(null);
 		setLocationRelativeTo(null);
 
 		logo = new JLabel();
-		logo.setBounds(5, 5, 100, 100);
+		logo.setBounds(32, 10, 100, 100);
 
-		ImageIcon img = new ImageIcon("src/co/edu/unbosque/view/image/rick.gif");
+		ImageIcon img = new ImageIcon("src/co/edu/unbosque/view/image/admin.png");
 		Icon icon = new ImageIcon(
 				img.getImage().getScaledInstance(logo.getWidth(), logo.getHeight(), Image.SCALE_DEFAULT));
 		logo.setIcon(icon);
 
 		panelAdmin = new JPanel();
 		panelAdmin.setLayout(null);
-		panelAdmin.setBounds(0, 0, 700, 700);
+		panelAdmin.setBounds(0, 0, 800, 500);
 		panelAdmin.setBackground(new Color(255, 255, 255));
 
-		panelActions = new JPanel();
-		panelActions.setLayout(null);
-		panelActions.setBounds(0, 0, 100, 700);
-		panelActions.setBackground(new Color(118, 116, 103));
+		panelSidebar = new JPanel();
+		panelSidebar.setLayout(null);
+		panelSidebar.setBounds(0, 0, 150, 500);
+		panelSidebar.setBackground(new Color(12, 116, 127));
+		
+		panel1 = new JPanel();
+		panel1.setLayout(null);
+		panel1.setBounds(150, 0, 650, 500);
+		panel1.setBackground(Color.green);
+		panel1.setVisible(false);
 
+		panel2 = new JPanel();
+		panel2.setLayout(null);
+		panel2.setBounds(150, 0, 650, 500);
+		panel2.setBackground(Color.yellow);
+		panel2.setVisible(false);
+		
+		
+		panel3 = new JPanel();
+		panel3.setLayout(null);
+		panel3.setBounds(150, 0, 650, 500);
+		panel3.setBackground(Color.blue);
+		panel3.setVisible(false);
 
+		panel4 = new JPanel();
+		panel4.setLayout(null);
+		panel4.setBounds(150, 0, 650, 500);
+		panel4.setBackground(Color.pink);
+		panel4.setVisible(false);
 
-		textTittle = new JLabel();
-		textTittle.setText("ADMIN");
-		textTittle.setBounds(20, 120, 100, 50);
-		textTittle.setForeground(Color.black);
-		textTittle.setFont(new Font("Tahoma", 0, 20));
-
-		textUser = new JLabel();
-		textUser.setText("xxxxx");
-		textUser.setBounds(80, 145, 50, 30);
-		textUser.setFont(new Font("Tahoma", Font.BOLD, 15));
-
-		fieldUser = new JTextField();
-		fieldUser.setBounds(80, 180, 320, 25);
-		fieldUser.setFont(new Font("Tahoma", 0, 15));
-		fieldUser.setBorder(null);
+		textAdmin = new JLabel();
+		textAdmin.setText("Administrator");
+		textAdmin.setBounds(25, 100, 110, 50);
+		textAdmin.setForeground(Color.WHITE);
+		textAdmin.setFont(new Font("Tahoma", Font.BOLD, 15));
 
 		line1 = new JSeparator();
-		line1.setBounds(80, 205, 320, 20);
-		line1.setForeground(Color.black);
+		line1.setBounds(25, 140, 100, 20);
+		line1.setForeground(Color.white);
 
-		textPassword = new JLabel();
-		textPassword.setText("Password");
-		textPassword.setBounds(80, 225, 90, 30);
-		textPassword.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnLogout = new JButton("Logout");
+		btnLogout.setBounds(25, 400, 100, 25);
+		btnLogout.setBackground(Color.red);
+		btnLogout.setForeground(new Color(255, 255, 255));
+		btnLogout.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnLogout.setBorderPainted(false);
 
-		fieldPassword = new JPasswordField();
-		fieldPassword.setBounds(80, 260, 320, 25);
-		fieldPassword.setFont(new Font("", 0, 25));
-		fieldPassword.setBorder(null);
-
-		line2 = new JSeparator();
-		line2.setBounds(80, 285, 320, 20);
-		line2.setForeground(Color.black);
-
-		btnLogin = new JButton("Login");
-		btnLogin.setBounds(150, 330, 180, 30);
-		btnLogin.setBackground(new Color(133, 153, 30));
-		btnLogin.setForeground(new Color(255, 255, 255));
-		btnLogin.setBorderPainted(false);
-
-		btnRegister = new JButton("Register");
-		btnRegister.setBounds(150, 380, 180, 30);
-		btnRegister.setBackground(new Color(212, 146, 52));
-		btnRegister.setForeground(new Color(255, 255, 255));
-		btnRegister.setBorderPainted(false);
+		
+		btn1 = new JButton("xxx");
+		btn1.setBounds(0, 175, 150, 30);
+		btn1.setBackground(new Color(12, 116, 127));
+		btn1.setForeground(new Color(255, 255, 255));
+		btn1.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btn1.setBorderPainted(false);
+		
+		btn2 = new JButton("xxx");
+		btn2.setBounds(0, 225, 150, 30);
+		btn2.setBackground(new Color(12, 116, 127));
+		btn2.setForeground(new Color(255, 255, 255));
+		btn2.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btn2.setBorderPainted(false);
+		
+		btn3 = new JButton("xxx");
+		btn3.setBounds(0, 275, 150, 30);
+		btn3.setBackground(new Color(12, 116, 127));
+		btn3.setForeground(new Color(255, 255, 255));
+		btn3.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btn3.setBorderPainted(false);
+		
+		btn4 = new JButton("xxx");
+		btn4.setBounds(0, 325, 150, 30);
+		btn4.setBackground(new Color(12, 116, 127));
+		btn4.setForeground(new Color(255, 255, 255));
+		btn4.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btn4.setBorderPainted(false);
 
 		add(panelAdmin);
-		panelAdmin.add(panelActions);
-		panelActions.add(textTittle);
-		panelActions.add(logo);
-		panelAdmin.add(textUser);
-		panelAdmin.add(fieldUser);
-		panelAdmin.add(line1);
-		panelAdmin.add(textPassword);
-		panelAdmin.add(fieldPassword);
-		panelAdmin.add(line2);
-		panelAdmin.add(btnLogin);
-		panelAdmin.add(btnRegister);
+		panelAdmin.add(panelSidebar);
+		panelSidebar.add(logo);
+		panelSidebar.add(textAdmin);
+		panelSidebar.add(line1);
+		panelSidebar.add(btn1);
+		panelSidebar.add(btn2);
+		panelSidebar.add(btn3);
+		panelSidebar.add(btn4);
+		panelSidebar.add(btnLogout);
+		panelAdmin.add(panel1);
+		panelAdmin.add(panel2);
+		panelAdmin.add(panel3);
+		panelAdmin.add(panel4);
 	}
+
+	public JPanel getPanelAdmin() {
+		return panelAdmin;
+	}
+
+	public void setPanelAdmin(JPanel panelAdmin) {
+		this.panelAdmin = panelAdmin;
+	}
+
+	public JPanel getPanelSidebar() {
+		return panelSidebar;
+	}
+
+	public void setPanelSidebar(JPanel panelSidebar) {
+		this.panelSidebar = panelSidebar;
+	}
+
+	public JPanel getPanel1() {
+		return panel1;
+	}
+
+	public void setPanel1(JPanel panel1) {
+		this.panel1 = panel1;
+	}
+
+	public JPanel getPanel2() {
+		return panel2;
+	}
+
+	public void setPanel2(JPanel panel2) {
+		this.panel2 = panel2;
+	}
+
+	public JPanel getPanel3() {
+		return panel3;
+	}
+
+	public void setPanel3(JPanel panel3) {
+		this.panel3 = panel3;
+	}
+
+	public JPanel getPanel4() {
+		return panel4;
+	}
+
+	public void setPanel4(JPanel panel4) {
+		this.panel4 = panel4;
+	}
+
+	public JLabel getTextAdmin() {
+		return textAdmin;
+	}
+
+	public void setTextAdmin(JLabel textAdmin) {
+		this.textAdmin = textAdmin;
+	}
+
+	public JLabel getLogo() {
+		return logo;
+	}
+
+	public void setLogo(JLabel logo) {
+		this.logo = logo;
+	}
+
+	public JButton getBtnLogout() {
+		return btnLogout;
+	}
+
+	public void setBtnLogout(JButton btnLogout) {
+		this.btnLogout = btnLogout;
+	}
+
+	public JButton getBtn1() {
+		return btn1;
+	}
+
+	public void setBtn1(JButton btn1) {
+		this.btn1 = btn1;
+	}
+
+	public JButton getBtn2() {
+		return btn2;
+	}
+
+	public void setBtn2(JButton btn2) {
+		this.btn2 = btn2;
+	}
+
+	public JButton getBtn3() {
+		return btn3;
+	}
+
+	public void setBtn3(JButton btn3) {
+		this.btn3 = btn3;
+	}
+
+	public JButton getBtn4() {
+		return btn4;
+	}
+
+	public void setBtn4(JButton btn4) {
+		this.btn4 = btn4;
+	}
+
+	public JSeparator getLine1() {
+		return line1;
+	}
+
+	public void setLine1(JSeparator line1) {
+		this.line1 = line1;
+	}
+	
+	
 }
