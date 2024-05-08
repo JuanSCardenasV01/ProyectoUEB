bdc9c8992e056c185ce372704602761377cdc974
biblioteca <biblioteca@UBACBIBP-059.unbosque.edu.co>
2024-05-08 13:04:55 -0500

    GUI REGISTER

--- ProyectoProgrmacion1/src/co/edu/unbosque/controller/Controlador.java
+++ ProyectoProgrmacion1/src/co/edu/unbosque/controller/Controlador.java
@@ -49,6 +49,8 @@ public class Controlador implements ActionListener {
 		vF.getVenPA().getBtn4().setActionCommand("btn4");
 		vF.getVenRe().getBtnRegister().addActionListener(this);
 		vF.getVenRe().getBtnRegister().setActionCommand("btnRegister");
+		vF.getVenRe().getComboBoxOcupation().addActionListener(this);
+		vF.getVenRe().getComboBoxOcupation().setActionCommand("comboBoxG");
 
 	}
 
@@ -57,86 +59,104 @@ public class Controlador implements ActionListener {
 		switch (e.getActionCommand()) {
 
 		case "btnLogin": {
-			 vF.getVenLog().setVisible(false);
-			    String user = vF.getVenLog().getFieldUser().getText();
-			    String password = vF.getVenLog().getFieldPassword().getText();
-			    UsuarioDTO usuario = mF.getUsuarioDAO().buscarUsuario(user, password); // Implementa este método en tu DAO
+			vF.getVenLog().setVisible(false);
+			String user = vF.getVenLog().getFieldUser().getText();
+			String password = vF.getVenLog().getFieldPassword().getText();
+			UsuarioDTO usuario = mF.getUsuarioDAO().buscarUsuario(user, password); // Implementa este método en tu DAO
 
-			    if (usuario != null) {
-			        if (user.equals("admin") && password.equals("123")) {
-			            vF.getVenPA().setVisible(true); // Mostrar panel de administrador
-			        } else {
-			        	JOptionPane.showMessageDialog(null, null, "Panel en Proceso", JOptionPane.INFORMATION_MESSAGE);
-			        }
-			    } else {
-			        JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos", "Error de inicio de sesión", JOptionPane.ERROR_MESSAGE);
-			        
-			    }
-			  break; 
+			if (usuario != null) {
+				if (user.equals("admin") && password.equals("123")) {
+					vF.getVenPA().setVisible(true); // Mostrar panel de administrador
+				} else {
+					JOptionPane.showMessageDialog(null, null, "Panel en Proceso", JOptionPane.INFORMATION_MESSAGE);
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos", "Error de inicio de sesión",
+						JOptionPane.ERROR_MESSAGE);
+
+			}
+			break;
 		}
-		case "btnVRegister":{
+		case "btnVRegister": {
 			vF.getVenLog().setVisible(false);
 			vF.getVenRe().setVisible(true);
 			break;
 		}
-		case "btnBack":{
+		case "btnBack": {
 			vF.getVenRe().setVisible(false);
 			vF.getVenLog().setVisible(true);
 			break;
 		}
-		case "btnLogout":{
+		case "btnLogout": {
 			vF.getVenPA().setVisible(false);
 			vF.getVenLog().setVisible(true);
 			break;
 		}
-		case "btn1":{
-            // Ocultar todos los paneles
-            vF.getVenPA().getPanel2().setVisible(false);
-            vF.getVenPA().getPanel3().setVisible(false);
-            vF.getVenPA().getPanel4().setVisible(false);
-            // Mostrar el panel correspondiente
-            vF.getVenPA().getPanel1().setVisible(true);
-            break;
-        }
-        case "btn2":{
-            // Ocultar todos los paneles
-            vF.getVenPA().getPanel1().setVisible(false);
-            vF.getVenPA().getPanel3().setVisible(false);
-            vF.getVenPA().getPanel4().setVisible(false);
-            // Mostrar el panel correspondiente
-            vF.getVenPA().getPanel2().setVisible(true);
-            break;
-        }
-        case "btn3":{
-            // Ocultar todos los paneles
-            vF.getVenPA().getPanel1().setVisible(false);
-            vF.getVenPA().getPanel2().setVisible(false);
-            vF.getVenPA().getPanel4().setVisible(false);
-            // Mostrar el panel correspondiente
-            vF.getVenPA().getPanel3().setVisible(true);
-            break;
-        }
-        case "btn4":{
-            // Ocultar todos los paneles
-            vF.getVenPA().getPanel1().setVisible(false);
-            vF.getVenPA().getPanel2().setVisible(false);
-            vF.getVenPA().getPanel3().setVisible(false);
-            // Mostrar el panel correspondiente
-            vF.getVenPA().getPanel4().setVisible(true);
-            break;
-        }
-        
-        case"btnRegister":{
-        	String nombre = vF.getVenRe().getFieldName().getText();
-        	String correo = vF.getVenRe().getFieldUser().getText();
-        	String contraseña = vF.getVenRe().getFieldPassword().getText();
-        	String genero = (String) vF.getVenRe().getComboBox().getSelectedItem();
-        	mF.getUsuarioDAO().create(new UsuarioDTO(genero, correo, contraseña, nombre));
-        	JOptionPane.showMessageDialog(null, null, "Usuario Creado", JOptionPane.INFORMATION_MESSAGE);
-	
-        }
-		
-		
+		case "btn1": {
+			// Ocultar todos los paneles
+			vF.getVenPA().getPanel2().setVisible(false);
+			vF.getVenPA().getPanel3().setVisible(false);
+			vF.getVenPA().getPanel4().setVisible(false);
+			// Mostrar el panel correspondiente
+			vF.getVenPA().getPanel1().setVisible(true);
+			break;
+		}
+		case "btn2": {
+			// Ocultar todos los paneles
+			vF.getVenPA().getPanel1().setVisible(false);
+			vF.getVenPA().getPanel3().setVisible(false);
+			vF.getVenPA().getPanel4().setVisible(false);
+			// Mostrar el panel correspondiente
+			vF.getVenPA().getPanel2().setVisible(true);
+			break;
+		}
+		case "btn3": {
+			// Ocultar todos los paneles
+			vF.getVenPA().getPanel1().setVisible(false);
+			vF.getVenPA().getPanel2().setVisible(false);
+			vF.getVenPA().getPanel4().setVisible(false);
+			// Mostrar el panel correspondiente
+			vF.getVenPA().getPanel3().setVisible(true);
+			break;
+		}
+		case "btn4": {
+			// Ocultar todos los paneles
+			vF.getVenPA().getPanel1().setVisible(false);
+			vF.getVenPA().getPanel2().setVisible(false);
+			vF.getVenPA().getPanel3().setVisible(false);
+			// Mostrar el panel correspondiente
+			vF.getVenPA().getPanel4().setVisible(true);
+			break;
+		}
+
+		case "comboBoxG": {
+			String selectedOcupation = (String) vF.getVenRe().getComboBoxOcupation().getSelectedItem();
+			switch (selectedOcupation) {
+			case "Masajista": {
+				vF.getVenRe().getPanelMasseur().setVisible(true);
+				vF.getVenRe().getPanelDirector().setVisible(false);
+				break;
+			}
+			case "Director": {
+				vF.getVenRe().getPanelMasseur().setVisible(false);
+				vF.getVenRe().getPanelDirector().setVisible(true);
+				break;
+			}
+			}
+
+			break;
+		}
+
+		case "btnRegister": {
+			String nombre = vF.getVenRe().getFieldName().getText();
+			String correo = vF.getVenRe().getFieldUser().getText();
+			String contraseña = vF.getVenRe().getFieldPassword().getText();
+			String genero = (String) vF.getVenRe().getComboBox().getSelectedItem();
+			mF.getUsuarioDAO().create(new UsuarioDTO(genero, correo, contraseña, nombre));
+			JOptionPane.showMessageDialog(null, null, "Usuario Creado", JOptionPane.INFORMATION_MESSAGE);
+
+		}
+
 		}
 
 	}
